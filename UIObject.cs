dc746cbc904c14216ea9abc23f705f5a9b056658
ChangWon <igotit.ck5005v@naver.com>
2017-09-27 20:50:24 +0900

    test

--- UIObject.cs
+++ UIObject.cs
@@ -7,7 +7,22 @@ using UnityEngine.UI;
 public class UIObject : MonoBehaviour
 {
 
- 
+    class UnitClass
+    {
+        public int Index;
+        public string Name;
 
+        //public UnitClass (int index, string name)
+        //{
+        //    Index = index;
+        //    Name = name;
+        //}
+
+    }
+
+    class Zerg : UnitClass
+    {
+
+    }
 
 }
