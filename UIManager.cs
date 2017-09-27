3f32f97abd85795b730c76f1d99d2cc0b8eabdd6
ChangWon <igotit.ck5005v@naver.com>
2017-09-27 20:41:34 +0900

    최초 스크립트 추가

---
+++ UIManager.cs
@@ -0,0 +1,153 @@
+﻿using UnityEngine;
+using System.Text;
+using System.Collections.Generic;
+
+public enum POPUP_TYPE { PAGE = 0, STACK, POPUP, FRONT }
+
+public class UIManager : MonoBehaviour
+{
+
+    private static UIManager _instance = null;
+    public static UIManager i
+    {
+        get
+        {
+            if (_instance == null)
+            {
+
+                var intance =FindObjectOfType<UIManager>();
+
+                if (intance != null)
+                {
+                    _instance = intance;
+
+                    return _instance;
+                }
+
+                _instance = Instantiate<UIManager>(Resources.Load<UIManager>("Prefabs/UI/UIManager"));
+            }
+
+            return _instance;
+        }
+    }
+
+    private Transform _worldTrans;
+    private Transform _pageTrans;
+    private Transform _stackTrans;
+    private Transform _popupTrans;
+    private Transform _frontTrans;
+
+
+    // 내가 string을 쓰는데 왜? const를 붙일까? 
+    private const string WorldUI = "WorldUI";
+    private const string PageUI = "PAGE";
+    private const string StackUI = "STACK";
+    private const string PopupUI = "POPUP";
+    private const string FrontUI = "FRONT";
+    private const string UIPath = "Prefabs/UI/";
+    private const string ButtonPath = "Prefabs/UI/Button/";
+    private const string SLASH = "/";
+
+    private StringBuilder _textbulider = new StringBuilder(256);
+    private List<UIPopupBase> uiObject_List = new List<UIPopupBase>();
+
+    void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+        FindSpecificTransform();
+        
+    }
+
+    private void FindSpecificTransform()
+    {
+        _worldTrans = transform.Find(WorldUI);
+        _pageTrans = transform.Find(PageUI);
+        _stackTrans = transform.Find(StackUI);
+        _popupTrans = transform.Find(PopupUI);
+        _frontTrans = transform.Find(FrontUI);
+    }
+
+    // 어떤 팝업이던지 이제 이렇게 만들겠다라는 함수  
+    public T CreatePopup<T>(POPUP_TYPE poptype) where T : UIPopupBase
+    {
+        _textbulider.Length = 0;
+        _textbulider.Append(UIPath);
+        _textbulider.Append(poptype.ToString());
+        _textbulider.Append(SLASH);
+        _textbulider.Append(typeof(T).ToString());
+
+        GameObject pop = Resources.Load(_textbulider.ToString()) as GameObject;
+
+        GameObject popup = Instantiate(pop);
+
+        switch (poptype)
+        {
+            case POPUP_TYPE.PAGE:
+                popup.transform.SetParent(_pageTrans, false);                
+                break;
+            case POPUP_TYPE.STACK:
+                popup.transform.SetParent(_stackTrans, false);
+                break;
+            case POPUP_TYPE.POPUP:
+                popup.transform.SetParent(_popupTrans, false);
+                break;
+            case POPUP_TYPE.FRONT:
+                popup.transform.SetParent(_frontTrans, false);
+                break;
+        }
+
+        var returnValue = popup.GetComponent<T>();
+
+        returnValue.TYPE = poptype;
+
+        uiObject_List.Add(returnValue);
+
+        return returnValue;
+    }
+
+    public T FindUIObject<T>() where T : UIPopupBase
+    {
+        T obj = uiObject_List.Find((c) => c is T) as T;
+
+        if (obj == null)
+        {
+            return null;
+        }
+
+        return obj.GetComponent<T>();
+
+    }
+
+    public void RemoveUIObject<T>() where T : UIPopupBase
+    {
+        T obj = uiObject_List.Find((c) => c is T) as T;
+
+        if (obj != null)
+        {
+            uiObject_List.Remove(obj);
+            Destroy(obj.gameObject);
+        }
+    
+    }
+
+    public void RemoveAllObject()
+    {
+        uiObject_List.FindAll((c) =>
+        {
+            if (c is FadeInOut)
+            {
+                return false;
+            }
+            else
+            {
+                if (c == null)
+                    return true;
+
+                Destroy(c.gameObject);
+                uiObject_List.Remove(c);
+                return true;
+            }
+        });
+    }
+
+}
---
+++ UIObject.cs
@@ -0,0 +1,131 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Events;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIObject : MonoBehaviour
+{
+
+    protected Text GetText(string textName)
+    {
+        var texts = GetComponentsInChildren<Text>();
+
+        for (int i = 0; i < texts.Length; i++)
+        {
+            if (texts[i].name == textName)
+                return texts[i];
+        }
+
+        return null;
+    }
+
+    protected Text GetText(string textChildName, string textName)
+    {
+        var child = transform.Find(textChildName);
+        var texts = child.GetComponentsInChildren<Text>();
+
+        for (int i = 0; i < texts.Length; i++)
+        {
+            if (texts[i].name == textName)
+                return texts[i];
+        }
+
+        return null;
+    }
+
+    protected Button GetButton(string buttonName)
+    {
+        var buttons = GetComponentsInChildren<Button>();
+
+        for (int i = 0; i < buttons.Length; i++)
+        {
+            if (buttons[i].name == buttonName)
+            {
+                buttons[i].onClick.RemoveAllListeners();
+                return buttons[i];
+            }
+        }
+
+        return null;
+    }
+
+    protected Button GetButton(string buttonChildName, string buttonName)
+    {
+        Transform child = transform.Find(buttonChildName);
+        var buttons = child.GetComponentsInChildren<Button>();
+
+        for (int i = 0; i < buttons.Length; i++)
+        {
+            if (buttons[i].name == buttonName)
+            {
+                buttons[i].onClick.RemoveAllListeners();
+                return buttons[i];
+            }
+        }
+        return null;
+    }
+
+
+    protected Button GetButton(Transform child, string buttonName)
+    {
+        var buttons = child.GetComponentsInChildren<Button>();
+
+        for (int i = 0; i < buttons.Length; i++)
+        {
+            if (buttons[i].name == buttonName)
+            {
+                buttons[i].onClick.RemoveAllListeners();
+                return buttons[i];
+            }
+        }
+
+        return null;
+    }
+
+    protected void GetButton(Transform child, string buttonName, UnityAction action)
+    {
+        var buttons = child.GetComponentsInChildren<Button>();
+
+        for (int i = 0; i < buttons.Length; i++)
+        {
+            if (buttons[i].name == buttonName)
+            {
+                buttons[i].onClick.RemoveAllListeners();
+                buttons[i].onClick.AddListener(action);
+            }
+        }
+    }
+
+
+
+    protected Image GetImage(string ImageChildName, string ImageName)
+    {
+        Transform child = transform.Find(ImageChildName);
+        var Images = child.GetComponentsInChildren<Image>();
+
+        for (int i = 0; i < Images.Length; i++)
+        {
+            if (Images[i].name == ImageName)
+                return Images[i];
+        }
+
+        return null;
+    }
+
+    protected Image GetImage(string ImageName)
+    {
+        var Images = GetComponentsInChildren<Image>();
+
+        for (int i = 0; i < Images.Length; i++)
+        {
+            if (Images[i].name == ImageName)
+                return Images[i];
+        }
+
+        return null;
+    }
+
+
+
+}
