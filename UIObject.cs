24e2ef5b521b9e6bafc6c8b80a023786ae0f56ed
ChangWon <igotit.ck5005v@naver.com>
2017-09-27 20:48:20 +0900

    ui object _

--- UIObject.cs
+++ UIObject.cs
@@ -7,7 +7,25 @@ using UnityEngine.UI;
 public class UIObject : MonoBehaviour
 {
 
- 
+
+    class UnitClass
+    {
+        public int Index;
+
+        public string _Name;
+
+        //public UnitClass (int index, string name)
+        //{
+        //    Index = index;
+        //    Name = name;
+        //}
+
+    }
+
+    public virtual void Initialize(string name)
+    {
+        //Debug.Log("DataTAble Initialize");
+    }
 
 
 }
